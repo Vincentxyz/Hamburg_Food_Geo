a80cb6ae2a5d2fe68fc99d78771cfda1fb3682d5
Vincentxyz <vincentmzw@gmail.com>
2019-02-09 16:50:28 +0100

    Water distances completed

--- python/2_transform/water_distances/merge_water_distances.py
+++ python/2_transform/water_distances/merge_water_distances.py
@@ -1,8 +1,47 @@
 import pandas as pd
 
+
+
+
+# Read in distances files
 path = 'C:/Users/vince_000/Documents/GitHub/Hamburg_Food_Geo/data/water_distances/'
+#
+#df_distances_582_1455 = pd.read_csv(path + 'distances_582_1455.csv')
+#df_distances_583_1455 = df_distances_582_1455.iloc[1:,:]
+#df_distances_583_1455.to_csv(path  + 'distances_583_1455.csv')
+
 df_distances_1_582 = pd.read_csv(path + 'distances_1_582.csv')
-df_distances_582_1455 = pd.read_csv(path + 'distances_582_1455.csv')
+df_distances_583_1455 = pd.read_csv(path + 'distances_583_1455.csv')
 df_distances_1456_2495 = pd.read_csv(path + 'distances_1456_2495.csv')
-df_distances_1456_2495 = df_distances_1456_2495.drop('no',axis=1)
 df_distances_2496_3909 = pd.read_csv(path + 'distances_2496_3909.csv')
+
+# Drop unnecessary columns
+df_distances_1456_2495 = df_distances_1456_2495.drop('no',axis=1)
+df_distances_583_1455 = df_distances_583_1455.drop('no',axis=1)
+df_distances_583_1455 = df_distances_583_1455.drop('Unnamed: 0',axis=1)
+df_distances_583_1455 = df_distances_583_1455.drop('Unnamed: 0.1',axis=1)
+df_distances_2496_3909 = df_distances_2496_3909.drop('no',axis=1)
+
+
+# Set indexes
+df_distances_1_582.set_index('Unnamed: 0', drop = True, inplace= True)
+df_distances_583_1455.set_index('id', drop = True, inplace= True)
+df_distances_1456_2495.set_index('Unnamed: 0', drop = True, inplace= True)
+df_distances_2496_3909.set_index('Unnamed: 0', drop = True, inplace= True)
+
+
+df_distances = df_distances_1_582.append(other = df_distances_583_1455,
+                        verify_integrity = True
+                        )
+
+
+
+df_distances = df_distances.append(other = df_distances_1456_2495,
+                        verify_integrity = True
+                        )
+
+df_distances = df_distances.append(other = df_distances_2496_3909,
+                        verify_integrity = True
+                        )
+
+df_distances.to_csv(path + 'all_water_distances.csv')
--- python/3_analyze/first_random_forest_analysis.py
+++ python/3_analyze/first_random_forest_analysis.py
@@ -2,12 +2,19 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
-df_restaurants = pd.read_csv('C:/Users/vince_000/Documents/Geotesting/Test_Files/Hamburg/CSV/Restaurants_in_Hamburg.csv')
-df_social_values = pd.read_csv('C:/Users/vince_000/Documents/Geotesting/Test_Files/Hamburg/CSV/Social_Values_2016_adjusted.csv')
+github_path = 'C:/Users/vince_000/Documents/GitHub/Hamburg_Food_Geo'
 
+df_restaurants = pd.read_csv(github_path + '/data/restaurants/Restaurants_in_Hamburg.csv')
+df_social_values = pd.read_csv(github_path + '/data/social_values/Social_Values_2016_adjusted.csv')
+df_water_distances = pd.read_csv(github_path + '/data/water_distances/all_water_distances.csv')
+df_water_distances = df_water_distances.rename(columns = {'Unnamed: 0' : 'id',
+                                                          'distance' : 'distance_to_water'})
 
 df_all = pd.merge(left=df_restaurants, right=df_social_values,
                 left_on='Stadtteil', right_on = 'Stadtgebiet')
+
+df_all = pd.merge(left = df_all, right = df_water_distances,
+                  left_on = 'id', right_on = 'id') 
 
 scaler = preprocessing.StandardScaler()
 
@@ -17,30 +24,35 @@ normalized_review_count =scaler.fit_transform(np.array(df_all['review_cou']).res
     
 df_all['success'] = normalized_rating + normalized_review_count
 
+
+
 #businesses['success'] = succes
 
 df_features = pd.DataFrame({
         'id': df_all['id'],
         'price' : df_all['price'].fillna('unknown'),
         
+        # Water distance
+        'distance_to_water' : df_all['distance_to_water'],
+        
         #social values
         'population': df_all['Bevölkerung'],
         'share_under_18_years_old': df_all['Unter 18-Jährige'],
-        '65_year_olds_and_older': df_all[' 65-Jährige und Ältere'],
+        #'65_year_olds_and_older': df_all[' 65-Jährige und Ältere'],
         'share_65_year_olds_and_older': df_all['Anteil der 65-Jährigen und Älteren in %'],
-        'foreigners': df_all['Ausländerinnen und Ausländer'],
+        #'foreigners': df_all['Ausländerinnen und Ausländer'],
         'share_foreigners': df_all['Ausländeranteil in %'],
-        'population_with_migration_background': df_all['Bevölkerung mit Migrations-hintergrund'],
+        #'population_with_migration_background': df_all['Bevölkerung mit Migrations-hintergrund'],
         'share_population_with_migration_background': df_all['Anteil der Bevölkerung mit Migrations-hintergrund in %'],
-        'below_18_year_olds_with_migration_background' : df_all['Unter 18-Jährige mit Migrations-hintergrund'],
+        #'below_18_year_olds_with_migration_background' : df_all['Unter 18-Jährige mit Migrations-hintergrund'],
         'share_below_18_year_olds_with_migration_background': df_all['Anteil der unter 18-Jährigen mit Migrations-hintergrund in %'],
         'households' : df_all['Haushalte'],
         'people_per_household' : df_all['Personen je Haushalt'],
-        'one_person_households' : df_all['Einpersonen-haushalte'],
+        #'one_person_households' : df_all['Einpersonen-haushalte'],
         'share_one_person_households' : df_all['Anteil der Einpersonen-haushalte in %'],
-        'households_with_children' : df_all['Haushalte mit Kindern'],
+        #'households_with_children' : df_all['Haushalte mit Kindern'],
         'share_households_with_children': df_all['Anteil der Haushalte mit Kindern in %'],
-        'single_parents' : df_all['Alleinerziehende'],
+        #'single_parents' : df_all['Alleinerziehende'],
         'share_households_with_single_parents' : df_all['Anteil der Haushalte von Alleinerziehenden in %'],
         'area_in_square_kms' : df_all['Fläche in km²'],
         'population_density' : df_all['Bevölkerungs-dichte'],
@@ -48,18 +60,18 @@ df_features = pd.DataFrame({
         'deaths': df_all['Sterbefälle'],
         'immigration': df_all['Zuzüge'],
         'emigration': df_all['Fortzüge'],
-        'migration_balance': df_all['Wanderungssaldo'],
+        #'migration_balance': df_all['Wanderungssaldo'],
         'insurable_employees': df_all['Sozial-versicherungs-pflichtig Beschäftigte (Dez 2016)'],
         'employment_quote_in_%': df_all['Beschäftigten-quote in % (Dez 2016)'],
-        'unemployed_people': df_all['Arbeitslose (Dez 2016)'],
+        #'unemployed_people': df_all['Arbeitslose (Dez 2016)'],
         'share_unemployed_people': df_all['Arbeitslosenanteil in % (Dez 2016)'],
-        'younger_unemployed_people' : df_all['Jüngere Arbeitslose (Dez 2016)'],
+        #'younger_unemployed_people' : df_all['Jüngere Arbeitslose (Dez 2016)'],
         'share_younger_unemployed_people': df_all['Arbeitslosenanteil Jüngerer in % (Dez 2016)'],
-        'older_unemployed_people': df_all['Ältere Arbeitslose (Dez 2016)'],
+        #'older_unemployed_people': df_all['Ältere Arbeitslose (Dez 2016)'],
         'share_older_unemployed_people': df_all['Arbeitslosenanteil Älterer in % (Dez 2016)'],
-        'service_recipients_per_SGB_II' : df_all['Leistungs-empfänger/-innen nach SGB II (Dez 2016)'],
+       # 'service_recipients_per_SGB_II' : df_all['Leistungs-empfänger/-innen nach SGB II (Dez 2016)'],
         'share_service_recipients_per_SGB_II': df_all['Anteil der Leistungs-empfänger/-innen nach SGB II in % (Dez 2016)'],
-        'under_15_year_olds_in_minimum_income': df_all['Unter 15-Jährige in Mindestsicherung (Dez 2016)'],
+        #'under_15_year_olds_in_minimum_income': df_all['Unter 15-Jährige in Mindestsicherung (Dez 2016)'],
         'share_under_15_year_olds_in_minimum_income': df_all['Anteil der unter 15-Jährigen in Mindestsicherung in % (Dez 2016)'],
         'shared_households_per_SGB_II': df_all['Bedarfs-gemeinschaften nach SGB II (Dez 2016)'],
         'tax_liable_people' : df_all['Lohn- und Einkommen-steuerpflichtige (2013)'],
@@ -73,7 +85,7 @@ df_features = pd.DataFrame({
         'living_area_per_resident_in_sqm' : df_all['Wohnfläche je Einwohner/-in in m² (2016)'],
         'social_homes': df_all['Sozialwohnungen (Jan 2017)'],
         'share_social_homes' : df_all['Sozialwohnungs-anteil in % (Jan 2017)'],
-        'social_homes_with_ends_of_commitment_until_2022' : df_all['Sozialwohnungen mit Bindungsauslauf bis 2022'],
+        #'social_homes_with_ends_of_commitment_until_2022' : df_all['Sozialwohnungen mit Bindungsauslauf bis 2022'],
         'share_social_homes_with_ends_of_commitment_until_2022': df_all['Sozialwohnungen mit Bindungsauslauf bis 2022 in %'],
         'prices_for_properties' : df_all['Preise für Grundstücke in EUR/m² (Jan 2017)'],
         'prices_for_one_or_two_family_houses_in_EUR/sqm' : df_all['Preise für Ein- bzw Zwei-familienhäuser in EUR/m² (Jan 2017)'],
@@ -89,14 +101,14 @@ df_features = pd.DataFrame({
         'pharmacys': df_all['Apotheken (Dez 2016)'],
         'private_cars': df_all['Private PKW (Jan 2017)'],
         'car_density' : df_all['PKW-Dichte (Jan 2017)']
+    
   })
 
 df_features.set_index('id', inplace=True,drop=True)
 
-
 # Add categories
 
-df_categories = pd.read_csv('C:/Users/vince_000/Documents/Geotesting/Test_Files/Hamburg/CSV/extract_categories.csv')
+df_categories = pd.read_csv(github_path + '/data/restaurant_categories/extract_categories.csv')
 
 
 from sklearn.preprocessing import LabelEncoder
@@ -115,7 +127,7 @@ for i in range(len(df_features.columns)):
     feature_column = pd.to_numeric(feature_column, errors='coerce')
     df_features[df_features.columns[i]] = feature_column
 
-#Fill missing data
+# Fill missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 df_features = imputer.fit_transform(df_features)
@@ -123,7 +135,7 @@ df_features = imputer.fit_transform(df_features)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df_features, df_all['success'], test_size = 0.2,random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(df_features, df_all['success'], test_size = 0.3,random_state = 0)
 
 # scale numerical values
 from sklearn.preprocessing import StandardScaler
@@ -134,7 +146,7 @@ X_test = sc_X.transform(X_test)
 
 # Fitting Random Forest Regression to the Training set
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor = RandomForestRegressor(n_estimators = 100, random_state = 0)
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
